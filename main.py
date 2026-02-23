2fa8724d34896a3b5d453cf3809f71b8c77568b5
Jung <83893730+ExoSteel@users.noreply.github.com>
2026-02-24 00:36:57 +0800

    Revert "Bro is using wildcard imports in the big 2026 smh"
    This reverts commit 11c2a241cd831c4f996e81ff39837f101299689c.

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import pygame as pg
-# import os, csv
-# from fonts.fontManager import h1, h2, h3, p
+from pygame.locals import *
+import os, csv
+from fonts.fontManager import h1, h2, h3, p
+from modules.objects import *
 from modules.finder import jsonReader, getLogicDetails
-import modules.objects
-import modules.menus
-import modules.save
-import modules.constants  # Why did bro put a pg.display.Info() somewhere after a
-
+from modules.constants import *
+from modules.menus import *
+from modules.save import *
 
 pg.init()
 pg.display.set_caption("Pygame Hardware Description Language")
 
 def drawBackground() -> None:
     # print(WIDTH, HEIGHT)
-    modules.constants.win.fill(modules.constants.BACKGROUND)
-    pg.draw.rect(modules.constants.win, modules.constants.BACKGROUND2, ((0, worldHeight - worldHeight * 0.18), (worldWidth, worldHeight)))
-    pg.draw.rect(modules.constants.win, modules.constants.BACKGROUND3, ((0, 0), (worldWidth * 0.1, worldHeight - worldHeight * 0.18)))
-    pg.draw.rect(modules.constants.win, modules.constants.BACKGROUND3, ((worldWidth * 0.9, 0), (worldWidth, worldHeight - worldHeight * 0.18)))
+    win.fill(BACKGROUND)
+    pg.draw.rect(win, BACKGROUND2, ((0, worldHeight - worldHeight*0.18), (worldWidth, worldHeight)))
+    pg.draw.rect(win, BACKGROUND3, ((0,0), (worldWidth * 0.1, worldHeight - worldHeight*0.18)))
+    pg.draw.rect(win, BACKGROUND3, ((worldWidth * 0.9,0), (worldWidth, worldHeight - worldHeight * 0.18)))
 
 def drawObjects(buttons:list[any, ...]=[], inPlugs:list[any, ...]=[], outPlugs:list[any, ...]=[], nodes:list[any, ...]=[], lines:list[any, ...]=[], onMenu:any=None, menuCoords:tuple[int,int]=(0,0)) -> None:
     for button in buttons:
         button.draw()
-
+        
     for inPlug in inPlugs:
         inPlug.draw()
 
@@ -30,13 +30,12 @@ def drawObjects(buttons:list[any, ...]=[], inPlugs:list[any, ...]=[], outPlugs:l
 
     for node in nodes:
         node.draw()
-
+    
     for line in lines:
         line.draw()
-
+    
     if onMenu:
         onMenu.draw(menuCoords[0], menuCoords[1])
-
 
 def detectInput() -> tuple[tuple[any, ...],tuple[any, ...],tuple[any, ...],tuple[any, ...]]:
     keyState = pg.key.get_pressed()
@@ -56,7 +55,7 @@ def findSelected(cursorCoords:tuple[int, int]=(0,0), buttons:list[any, ...]=[], 
     for menu in menus:
         if menu.checkCursor(cursorCoords):
             for widget in menu.widgets:
-                if type(widget) == modules.menus.ColourPicker:
+                if type(widget) == ColourPicker:
                     for colourBox in widget.boxes:
                         # print(colourBox.checkCursor(cursorCoords, menu.width, menu.height))
                         if colourBox.checkCursor(cursorCoords, menu.width, menu.height):
@@ -77,10 +76,10 @@ def findSelected(cursorCoords:tuple[int, int]=(0,0), buttons:list[any, ...]=[], 
 
     return None
 
-def isValidWiring(selectedPlug: [modules.objects.Plug, modules.objects.InputPlug, modules.objects.OutputPlug] = None, newWire: modules.objects.Wire = None) -> modules.objects.Wire:
-    if type(selectedPlug) == modules.objects.InputPlug:
+def isValidWiring(selectedPlug:[Plug,InputPlug,OutputPlug]=None, newWire:Wire=None) -> Wire:
+    if type(selectedPlug) == InputPlug:
         newWire.start = selectedPlug
-    elif type(selectedPlug) == modules.objects.OutputPlug:
+    elif type(selectedPlug) == OutputPlug:
         newWire.end = selectedPlug
     else:
         if type(newWire.start) == tuple:
@@ -118,30 +117,30 @@ def cleanReset():
 
 def initialise(data=list[any, ...]) -> tuple[list,list,list,list,list]:
     nodes, inPlugs, outPlugs, buttons, lines, menus = [], [], [], [], [], []
-    # node1 = modules.objects.Node(modules.constants.RED, WIDTH * 0.1, WIDTH * 0.075, WIDTH * 0.25, HEIGHT * 0.01, 2, 1, "AND", "")
-    # node2 = modules.objects.Node(BLUE, WIDTH * 0.1, WIDTH * 0.075, WIDTH * 0.12, HEIGHT * 0.1, 1, 1, "OR", "")
+    # node1 = Node(RED, WIDTH * 0.1, WIDTH * 0.075, WIDTH * 0.25, HEIGHT * 0.01, 2, 1, "AND", "")
+    # node2 = Node(BLUE, WIDTH * 0.1, WIDTH * 0.075, WIDTH * 0.12, HEIGHT * 0.1, 1, 1, "OR", "")
     # nodes.insert(0, node1)
     # nodes.insert(1, node2)
-
-    inputPlug = modules.objects.InputPlug(modules.constants.BLACK, worldWidth * 0.01, worldWidth * 0.1, worldHeight * 0.33)
-    inputPlug2 = modules.objects.InputPlug(modules.constants.BLACK, worldWidth * 0.01, worldWidth * 0.1, worldHeight * 0.66)
+    
+    inputPlug = InputPlug(BLACK, worldWidth * 0.01, worldWidth * 0.1, worldHeight * 0.33)
+    inputPlug2 = InputPlug(BLACK, worldWidth * 0.01, worldWidth * 0.1, worldHeight * 0.66)
     inPlugs.append(inputPlug)
     inPlugs.append(inputPlug2)
-
-    outputPlug = modules.objects.OutputPlug(modules.constants.BLACK, worldWidth * 0.01, worldWidth * 0.9, worldHeight * 0.33)
-    outputPlug2 = modules.objects.OutputPlug(modules.constants.BLACK, worldWidth * 0.01, worldWidth * 0.9, worldHeight * 0.66)
+    
+    outputPlug = OutputPlug(BLACK, worldWidth * 0.01, worldWidth * 0.9, worldHeight * 0.33)
+    outputPlug2 = OutputPlug(BLACK, worldWidth * 0.01, worldWidth * 0.9, worldHeight * 0.66)
     outPlugs.append(outputPlug)
 
-    saveMenu = modules.menus.SaveMenu("Save", modules.constants.WHITE, worldWidth * 0.4, worldHeight * 0.7)
+    saveMenu = SaveMenu("Save", WHITE, worldWidth * 0.4, worldHeight * 0.7)
     menus.insert(0, saveMenu)
 
     for ind, logic in enumerate(data):
         colour = tuple([int(i) for i in logic[1].split(",")])
-        button = modules.objects.NodeButton(text=logic[0], colour=colour, width=(worldWidth * 0.06), height=(worldWidth * 0.04), x=(worldWidth * 0.01 + worldWidth * 0.07 * (ind)), y=(worldHeight * 0.845), data=logic)
+        button = NodeButton(text=logic[0], colour=colour, width=(worldWidth * 0.06), height=(worldWidth * 0.04), x=(worldWidth * 0.01 + worldWidth * 0.07 * (ind)), y=(worldHeight * 0.845), data=logic)
         buttons.insert(0, button)
-    buttonInput = modules.objects.PlugButton("+", (100, 100, 100), worldWidth * 0.05, worldWidth * 0.05, worldWidth * 0.02, worldHeight * 0.01, "input")
-    buttonOutput = modules.objects.PlugButton("+", (100, 100, 100), worldWidth * 0.05, worldWidth * 0.05, worldWidth * 0.92, worldHeight * 0.01, "output")
-    buttonSave = modules.objects.SaveButton("Save", modules.constants.RED, worldWidth * 0.10, worldHeight * 0.10, worldWidth * 0.85, worldHeight * 0.85, saveMenu)
+    buttonInput = PlugButton("+", (100,100,100), worldWidth * 0.05, worldWidth * 0.05, worldWidth * 0.02, worldHeight * 0.01, "input")
+    buttonOutput = PlugButton("+", (100,100,100), worldWidth * 0.05, worldWidth * 0.05, worldWidth * 0.92, worldHeight * 0.01, "output")
+    buttonSave = SaveButton("Save", RED, worldWidth * 0.10, worldHeight * 0.10, worldWidth * 0.85, worldHeight * 0.85, saveMenu)
     
     buttons.insert(0, buttonInput)
     buttons.insert(0, buttonOutput)
@@ -165,17 +164,17 @@ def resizeGUI(info:any, objects:list[any, ...]) -> None:
     for obj in objects:
         obj.x *= percentDiffWidth
         obj.y *= percentDiffHeight
-        if type(obj) == modules.objects.InputPlug:
+        if type(obj) == InputPlug:
             obj.radius *= ((percentDiffWidth + percentDiffHeight) / 2)
             obj.switchRadius *= ((percentDiffWidth + percentDiffHeight) / 2)
             obj.switchX *= percentDiffWidth
             obj.switchY *= percentDiffHeight
-        elif type(obj) == modules.objects.OutputPlug:
+        elif type(obj) == OutputPlug:
             obj.radius *= ((percentDiffWidth + percentDiffHeight) / 2)
             obj.lampRadius *= ((percentDiffWidth + percentDiffHeight) / 2)
             obj.lampX *= percentDiffWidth
             obj.lampY *= percentDiffHeight
-        elif type(obj) in [modules.objects.Plug, modules.objects.InputPlug, modules.objects.OutputPlug]:
+        elif type(obj) in [Plug, InputPlug, OutputPlug]:
             obj.radius *= ((percentDiffWidth + percentDiffHeight) / 2)
         else:
             obj.width *= percentDiffWidth
@@ -212,78 +211,78 @@ def main() -> None:
         keyState, cursorClicks, cursorCoords, cursorCoordsRel = detectInput()
         
         for event in pg.event.get():
-            if event.type == pg.QUIT:
+            if event.type == QUIT:
                 running = False
-            elif event.type == pg.KEYDOWN:
-                print("hay", cursorCoords, cursorClicks, cursorCoordsRel, f"{clock.get_fps():.0f}") if event.key == pg.K_w else None
-                if event.key == pg.K_ESCAPE:
+            elif event.type == KEYDOWN:
+                print("hay", cursorCoords, cursorClicks, cursorCoordsRel, f"{clock.get_fps():.0f}") if event.key == K_w else None
+                if event.key == K_ESCAPE:
                     print("exit")
                     running = False
                 elif isTyping:
                     # print(event.unicode)
-                    if event.key == pg.K_BACKSPACE:
+                    if event.key == K_BACKSPACE:
                         userInput = userInput[:-1]
                         isTyping.value = isTyping.value[:-1]
                     else:    
                         userInput += event.unicode
                         isTyping.value += event.unicode
 
-            elif event.type == pg.MOUSEBUTTONDOWN:
+            elif event.type == MOUSEBUTTONDOWN:
                 # Find Selected
                 selected = findSelected(cursorCoords, buttons, nodes, inPlugs, outPlugs, lines, menus)
                 # print(selected)
 
                 # Actions (Left Click)
                 if event.button == 1:
-                    if type(selected) == modules.menus.SaveMenu:
+                    if type(selected) == SaveMenu:
                         onMenu = selected
-                    elif isinstance(selected, modules.menus.Widget):
+                    elif isinstance(selected, Widget):
                         pass
-                    elif type(selected) == modules.objects.SaveButton:
+                    elif type(selected) == SaveButton:
                         menuCoords = (cursorCoords[0]-selected.menu.width, cursorCoords[1]-selected.menu.height)
                         onMenu = selected.menu
                     else:
-                        if type(prevSelected) == modules.menus.SaveMenu:
+                        if type(prevSelected) == SaveMenu:
                             prevSelected.x = None
                             prevSelected.y = None
 
                         onMenu = None
 
-                    if type(selected) == modules.objects.NodeButton:
+                    if type(selected) == NodeButton:
                         node = selected.createNode()
                         nodes.append(node)
                         for inp in node.inputs:
                             inPlugs.append(inp)
                         for out in node.outputs:
                             outPlugs.append(out)
-                    elif type(selected) == modules.objects.PlugButton:
+                    elif type(selected) == PlugButton:
                         selected.createPlug(inPlugs if selected.plugType == "input" else outPlugs)
-                    elif type(selected) == modules.objects.InputPlug:
+                    elif type(selected) == InputPlug:
                         if selected.checkCursor(cursorCoords) == 2:
                             selected.isActivated = False if selected.isActivated else True
                             # print("Activated:", selected.isActivated)
                         else:
-                            newWire = modules.objects.Wire(modules.constants.BLACK, selected, cursorCoords)
+                            newWire = Wire(BLACK, selected, cursorCoords)
                             lines.append(newWire)
                             isWiring = True
-                    elif type(selected) == modules.objects.OutputPlug:
-                        newWire = modules.objects.Wire(modules.constants.BLACK, cursorCoords, selected)
+                    elif type(selected) == OutputPlug:
+                        newWire = Wire(BLACK, cursorCoords, selected)
                         lines.append(newWire)
                         isWiring = True
-                    elif type(selected) == modules.objects.Plug and selected in inPlugs:
-                        newWire = modules.objects.Wire(modules.constants.BLACK, cursorCoords, selected)
+                    elif type(selected) == Plug and selected in inPlugs:
+                        newWire = Wire(BLACK, cursorCoords, selected)
                         lines.append(newWire)
                         isWiring = True
-                    elif type(selected) == modules.objects.Plug and selected in outPlugs:
-                        newWire = modules.objects.Wire(modules.constants.BLACK, selected, cursorCoords)
+                    elif type(selected) == Plug and selected in outPlugs:
+                        newWire = Wire(BLACK, selected, cursorCoords)
                         lines.append(newWire)
                         isWiring = True
-                    elif type(selected) == modules.menus.InputBox:
+                    elif type(selected) == InputBox:
                         userInput = selected.value
                         isTyping = selected
                         # print(userInput)
-                    elif type(selected) == modules.menus.SaveButtonWidget:
-                        newButton = modules.save.saveLogic(nodes, inPlugs, outPlugs, lines, onMenu)
+                    elif type(selected) == SaveButtonWidget:
+                        newButton = saveLogic(nodes, inPlugs, outPlugs, lines, onMenu)
                         if newButton != None:
                             cleanReset(buttons, nodes, inPlugs, outPlugs, lines, menus)
                     else:
@@ -296,15 +295,15 @@ def main() -> None:
                     pass 
                 # Actions (Right Click)
                 elif event.button == 3:
-                    if type(selected) == modules.objects.PlugButton:
+                    if type(selected) == PlugButton:
                         # Delete an input/output plug WIP
                         pass
-                    elif type(selected) == modules.objects.Node:
+                    elif type(selected) == Node:
                         nodeIndex = nodes.index(selected)
                         deleteConnections(lines, inPlugs, outPlugs, selected)
                         nodes.pop(nodes.index(selected))
 
-            elif event.type == pg.MOUSEBUTTONUP:
+            elif event.type == MOUSEBUTTONUP:
                 prevSelected = selected
                 
                 if isWiring:
@@ -324,30 +323,30 @@ def main() -> None:
                 selected = None
 
         # Moving nodes
-        if type(selected) == modules.objects.Node:
+        if type(selected) == Node:
             selected.x += cursorCoordsRel[0]
             selected.y += cursorCoordsRel[1]
 
         # Check if hovering over plugs or activated
         for plug in inPlugs + outPlugs:
             if not plug.checkCursor(cursorCoords):
-                if type(plug) == modules.objects.InputPlug:
-                    plug.switchColour = modules.constants.BLACK
-
-                plug.colour = modules.constants.RED if plug.isActivated else modules.constants.BLACK
+                if type(plug) == InputPlug:
+                    plug.switchColour = BLACK
+                
+                plug.colour = RED if plug.isActivated else BLACK
             if plug.checkCursor(cursorCoords):
                 if plug.checkCursor(cursorCoords) == 2:
-                    plug.switchColour = modules.constants.GREEN
-                plug.colour = modules.constants.GREEN
+                    plug.switchColour = GREEN
+                plug.colour = GREEN
 
         # Check lines
         for line in lines:
             # print(line.start, line.end)
-            if type(line.start) in [modules.objects.Plug, modules.objects.InputPlug] and type(line.end) in [modules.objects.Plug, modules.objects.OutputPlug]:
+            if type(line.start) in [Plug, InputPlug] and type(line.end) in [Plug, OutputPlug]:
                 line.checkCurrent()
-            elif type(line.start) in [modules.objects.Plug, modules.objects.InputPlug] and type(line.end) == tuple:
+            elif type(line.start) in [Plug, InputPlug] and type(line.end) == tuple:
                 line.end = cursorCoords
-            elif type(line.end) in [modules.objects.Plug, modules.objects.OutputPlug] and type(line.start) == tuple:
+            elif type(line.end) in [Plug, OutputPlug] and type(line.start) == tuple:
                 line.start = cursorCoords
 
         # Calculate node logic
